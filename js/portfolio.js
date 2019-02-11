7e351909d7a40da79b5cfd91331461b0cf46e58d
dhr1325 <47517518+dhr1325@users.noreply.github.com>
2019-02-11 17:49:19 +0900

    Update portfolio.js

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,5 +1,4 @@
-
-    $(function() {
+$(function() {
         var h = $(window).height();
         $('body').css('display', 'none');
         $('#loader-bg ,#loader').height(h).css('display', 'block');
@@ -16,22 +15,20 @@ $(function(){
 
 	$('.keyvisual').vegas({ //背景画像でスライドショーしたい場所の設定
     slides: [
-    { src: 'images/neon.jpg' },
-    { src: 'images/water.jpg' },
+    { src: 'images/key01.jpg' },
+    { src: 'images/key02.jpg' },
+    { src: 'images/key03.jpg' },
+    { src: 'images/key04.jpg' },
      { src: 'images/season2.jpg' }, //スライドする画像を配列で設定
-     { src: 'images/season3.jpg' },
-     { src: 'images/season4.jpg'}
+     { src: 'images/season3.jpg' }
     ],
      delay: 5000, //スライドまでの時間ををミリ秒単位で設定
      timer: true, //タイマーバーの表示/非表示を切り替え
-     // overlay: 'https://cdnjs.cloudflare.com/ajax/libs/vegas/2.4.0/overlays/07.png', //オーバーレイする画像の設定
+     overlay: 'https://cdnjs.cloudflare.com/ajax/libs/vegas/2.4.0/overlays/01.png', //オーバーレイする画像の設定
      animation: 'kenburns', //スライドのアニメーションを設定
      transition: 'fade2', //スライド間のエフェクトを設定
      transitionDuration: 1000 //エフェクト時間をミリ秒単位で設定
   	}); 
-
-	$('#keytext').fadeIn(6000);
-
 
 	$('#bar').click(function(){
 		$('.headernav').slideDown();
@@ -53,4 +50,8 @@ $(function(){
             }
         });
     });
+});
+
+$(function(){
+    $('#key-text').fadeIn(8000);
 });
